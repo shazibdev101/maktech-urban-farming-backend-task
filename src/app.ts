6ac9377e5707fd8114399e8350fae6dfc77e862b
SMTECHSOFT <shazibahmed101@gmail.com>
2026-04-17 14:34:30 +0600

    added missing config files

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1", routes);
+
+app.get("/", (req, res) => {
+  res.send("Server running 🚀");
+});
+
+export default app;
